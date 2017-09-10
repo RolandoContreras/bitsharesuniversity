7f11a2c561457c61f544835fe99790179bc3fe19
software.contreras@gmail.com <software.contreras@gmail.com>
2017-09-10 03:00:07 -0500

    add text on home

--- application/views/home.php
+++ application/views/home.php
@@ -14,7 +14,7 @@
           <div class="col-md-10 col-md-offset-2">
             <div class="block">
                 <h1 class="animated fadeInUp"><?php echo replace_vocales_voculeshtml("VIAJE &#38; ENTRÉNATE");?><br>GANANDO DINERO</h1>
-                <p class="animated fadeInUp"><?php echo replace_vocales_voculeshtml("En BITSHARES UNIVERSITY creemos en un todo, no solamente las finanzas sino también el desarrollo como persona.");?></p>
+                <p class="animated fadeInUp"><?php echo replace_vocales_voculeshtml("En BITSHARES UNIVERSITY creemos en un todo, no solamente <br/>en las finanzas sino también el desarrollo como persona.");?></p>
             </div>
           </div>
         </div>
@@ -27,15 +27,15 @@
           <div class="col-md-7 col-sm-12">
             <div class="block">
               <div class="section-title">
-                <h2>About Us</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+                <h2>Acerca</h2>
+                <p><?php echo replace_vocales_voculeshtml("A través de un estudio de mercado logramos identificar las 3 áreas que todo ser humano debería desarrollar para llegar a tener un vida de plenitud.")?></p>
               </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id </p>
+                <p><?php echo replace_vocales_voculeshtml("BITSHARES UNIVERSITY es la primera universidad virtual que te ayudará a crecer en tres aspectos y áreas de tu vida: viajes, desarrollo personal y finanzas. Sabemos que ninguna área antes mencionada puede ser independiente o autónoma a la otra, todo es integral y parte del cuerpo. Contamos con los mejores coach, maestros y financistas que te impulsarán a llegar a tus metas en un periodo de tiempo corto. Si nunca has tenido la oportunidad de llegar hacia el próximo nivel, permítenos que te mostremos el camino hacia el éxito.")?></p>
             </div>
           </div><!-- .col-md-7 close -->
           <div class="col-md-5 col-sm-12">
             <div class="block">
-                <img src="<?php echo site_url().'static/page_front/img/wrapper-img.gif';?>" alt="Img">
+                <img src="<?php echo site_url().'static/page_front/img/wrapper-img.gif';?>" alt="evolution">
             </div>
           </div><!-- .col-md-5 close -->
         </div>
@@ -46,11 +46,11 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 col-md-offset-6">
-          <h2>WE BELIEVE IN GREAT IDEAS</h2>
-          <p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <a href="work.html" class="btn btn-view-works">View Works</a>
+          <h2>CREEMOS EN GRANDES IDEAS</h2>
+          <p><?php echo replace_vocales_voculeshtml("Todo nace de una idea y ahora es una realidad BITSHARES UNIVERSITY se crea con un fin de ayudar a las personas. Tenemos todas las herramientas para ayudarte  llegar a el siguiente nivel.");?></p>
+          <p><?php echo replace_vocales_voculeshtml("En el mundo globalizado en el cual vivimos no es necesario salir a un centro de estudio, o dejar a tus seres queridos, desde la comodidad de tu hogar a través de cualquier dispositivo conectado a internet podrás tener acceso a todo el material de estudio que te ayudara a crecer.");?></p>
+          <p><?php echo replace_vocales_voculeshtml("Sé parte de esta gran revolución y verás un cambio radical en tu cotidiano vivir y en tu vida entera.");?></p>
+          <a href="#" class="btn btn-view-works"><?php echo replace_vocales_voculeshtml("Ver Más");?></a>
         </div>
       </div>
     </div>
@@ -61,65 +61,65 @@
       <div class="container">
         <div class="row">
           <div class="section-title">
-            <h2>Our Services</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+              <h2><?php echo replace_vocales_voculeshtml("¿Por Qué Elegirnos?");?></h2>
+              <!--<p><?php echo replace_vocales_voculeshtml("Muchas características engloban el concepto BITSHARE UNIVERSITY y por el cual las personas nos elijen, aquí te mencionamos algunas y porque tu deberías ser parte de nosotros.");?></p>-->
           </div>
         </div>
         <div class="row ">
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="icon ion-coffee"></i>
-              <h4>Branding</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4>Comfort</h4>
+              <p><?php echo replace_vocales_voculeshtml("Podrás acceder a todos módulos de educación y finanzas desde la comodidad de tu hogar.");?></p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-compass"></i>
-              <h4>Web Design</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4>24/7</h4>
+              <p><?php echo replace_vocales_voculeshtml("Videos, audios, libros y clases en vivo las 24 horas, los 7 días de la semana.");?></p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-image"></i>
-              <h4>App Design</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4><?php echo replace_vocales_voculeshtml("Diseño amigable");?></h4>
+              <p><?php echo replace_vocales_voculeshtml("Entenderás perfectamente el manejo del sistema de educación y las herramientas.");?></p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-bug"></i>
-              <h4>Start Up</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4><?php echo replace_vocales_voculeshtml("Puesta en marcha");?></h4>
+              <p><?php echo replace_vocales_voculeshtml("Una vez que inicias el trabajo con nosotros, tendrás a coach y maestros que no te dejarán hasta terminar los módulos.");?></p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-headphone"></i>
-              <h4>Logo Design</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4>Audios</h4>
+              <p><?php echo replace_vocales_voculeshtml("Si te perdiste alguna clases, todo será guardado para lo puedas escucharlo posteriormente.");?></p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-leaf"></i>
-              <h4>Development</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4>Desarrollo</h4>
+              <p><?php echo replace_vocales_voculeshtml("Desarrollamos un programa comprobado por líderes mundiales y cientos de personas.");?></p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-planet"></i>
-              <h4>Brand Identity</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4>Global</h4>
+              <p>Plataforma global que te permite desarrollar los cursos y viajes desde cualquier lugar del mundo.</p>
             </div>
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="service-item">
               <i class="ion-earth"></i>
-              <h4>Brand Identity</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+              <h4>Interrelaciones</h4>
+              <p><?php echo replace_vocales_voculeshtml("Encontrarás cientos de personas y tendrás miles de oportunidades en cada una de ellas.");?></p>
             </div>
           </div>
         </div>
